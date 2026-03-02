d1c2046587c293600030260271d70df467ab265d
SUPATKN <yacht1400@gmail.com>
2026-03-02 09:06:22 +0700

    Chore : add Dockerfile , config minio
    (cherry picked from commit 33f3f41cda614d4f438d1d7daa94aa8209801141)

--- room-navigation-service/src/main/java/com/app/room_navigation_service/config/MinioConfig.java
+++ room-navigation-service/src/main/java/com/app/room_navigation_service/config/MinioConfig.java
@@ -2,17 +2,27 @@ package com.app.room_navigation_service.config;
 
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfig {
 
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint("http://localhost:9000")
-                .credentials("minio", "minio123")
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
                 .build();
     }
 }
